21fa964952e36b009ed51bba41b3036172a116c5
Kaps2611 <kapildamodare2619@gmail.com>
2024-09-09 12:12:53 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ mongoose.connect('mongodb://localhost/google-docs-clone',{
 });
 const io = require('socket.io')(3001,{
   cors:{
-    origin:  'http://localhost:3000',
+    origin:  'http://localhost:4000',
     methods: ["GET","POST"]
   }          //it will help to send req from one url to other url
 })
